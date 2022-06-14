0395aee0bf3048fd2bbd5e45728c327973c53254
GitHub Action <action@github.com>
2022-06-14 11:59:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/itning/yunshuhttpclientplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.itning.yunshuhttpclientplugin.services
+
+import com.github.itning.yunshuhttpclientplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/itning/yunshuhttpclientplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/itning/yunshuhttpclientplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.itning.yunshuhttpclientplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.itning.yunshuhttpclientplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
